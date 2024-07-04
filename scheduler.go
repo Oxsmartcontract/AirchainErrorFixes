20ba6d6459a29f3a623e887a32fe9a80ed93c03e
Oxsmartcontract <94295704+Oxsmartcontract@users.noreply.github.com>
2024-07-04 16:32:47 +0330

    Update scheduler.go

--- scheduler.go
+++ scheduler.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+    "bufio"
+    "fmt"
     "log"
+    "net/http"
+    "net/url"
     "os"
     "os/exec"
     "time"
 )
+
+func getInput(prompt string) string {
+    fmt.Print(prompt)
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    return scanner.Text()
+}
+
+func sendTelegramMessage(botToken, chatID, message string) error {
+    apiURL := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
+    values := url.Values{
+        "chat_id": {chatID},
+        "text":    {message},
+    }
+
+    resp, err := http.PostForm(apiURL, values)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return fmt.Errorf("failed to send message: %s", resp.Status)
+    }
+    return nil
+}
 
 func runCommand(name string, args ...string) error {
     cmd := exec.Command(name, args...)
@@ -14,7 +44,7 @@ func runCommand(name string, args ...string) error {
     return cmd.Run()
 }
 
-func executeCommands() {
+func executeCommands() error {
     commands := [][]string{
         {"sudo", "systemctl", "stop", "stationd"},
         {"cd", "tracks"},
@@ -28,22 +58,35 @@ func executeCommands() {
     for _, cmd := range commands {
         if len(cmd) > 1 && cmd[0] == "cd" {
             if err := os.Chdir(cmd[1]); err != nil {
-                log.Fatalf("failed to change directory: %v", err)
+                return fmt.Errorf("failed to change directory: %v", err)
             }
         } else {
             if err := runCommand(cmd[0], cmd[1:]...); err != nil {
-                log.Printf("failed to execute command: %v", err)
+                return fmt.Errorf("failed to execute command %s: %v", cmd[0], err)
             }
         }
     }
+    return nil
 }
 
 func main() {
-    ticker := time.NewTicker(20 * time.Minute)
+    botToken := getInput("Enter your Telegram bot token: ")
+    chatID := getInput("Enter your Telegram chat ID: ")
+
+    ticker := time.NewTicker(40 * time.Minute)
     defer ticker.Stop()
 
     for {
-        executeCommands()
+        err := executeCommands()
+        if err != nil {
+            errMsg := fmt.Sprintf("Error running commands: %v", err)
+            log.Println(errMsg)
+            sendTelegramMessage(botToken, chatID, errMsg)
+        } else {
+            successMsg := "All commands executed successfully!"
+            log.Println(successMsg)
+            sendTelegramMessage(botToken, chatID, successMsg)
+        }
         <-ticker.C
     }
 }
